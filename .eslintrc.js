6fe26f4c837de9d240d23185ae3af84e1101ad5f
Jakir Hossain <newjakir@gmail.com>
2025-07-05 14:22:57 +0600

    eslin error fixed

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'vue/no-unused-components': 'warn',
+    'vue/valid-template-root': 'error',
+    'vue/no-dupe-keys': 'error',
+    'no-unused-vars': 'warn',
+    'no-undef': 'error',
+
+  },
+};
