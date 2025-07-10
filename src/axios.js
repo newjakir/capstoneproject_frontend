f566e80b293a6640dfdd65278e8381e9d7b79e9f
Jakir Hossain <newjakir@gmail.com>
2025-07-11 02:53:08 +0600

    update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || 'http://localhost:8000/api', 
+  baseURL: process.env.VUE_APP_API_URL || 'http://139.59.222.237:8000/api/', 
 });
 
 export default api;
